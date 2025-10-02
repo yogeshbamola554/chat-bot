4dabd686a8ca9eaa9215750433adfc7a14b30ac3
Yogesh Sharma <yogeshbamola554@gmail.com>
2025-10-02 14:10:15 +0530

    Integrate Authentication System

--- chatbot/app/services/llm/service.py
+++
@@ -1,7 +0,0 @@
-from .config import llm
-from .prompts import structured_prompt
-
-def process_user_message(user_message: str) -> dict:
-    messages = structured_prompt.format_messages(user_input=user_message)
-    response = llm(messages)
-    return response.content
--- chatbot/app/views.py
+++ chatbot/app/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from .state_machine import ChatStateMachine
 from .models import User
 from .services.chat_service import get_chat_history
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
-from .services.llm.service import process_user_message
+
 
 state_machine = ChatStateMachine()
 
@@ -39,12 +36,3 @@ def chat_page(request):
         "bot_reply": bot_reply, 
         "history": history
     })
-
-@csrf_exempt
-def llm_query(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        user_message = data.get("message", "")
-        response = process_user_message(user_message)
-        return JsonResponse({"response": response})
-    return JsonResponse({"error": "Only POST allowed"}, status=405)
